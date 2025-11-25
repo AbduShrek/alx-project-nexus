0edc6c7b7c65d1283c4f274f996e72290c7bee5c
AbduShrek <abdu.shrek.at@gmail.com>
2025-11-25 12:11:53 +0200

    Add Swagger/OpenAPI documentation with drf-yasg, and updating routing...

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,17 +21,44 @@ from products.views import CategoryViewSet, ProductViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #JWT Auth
 from accounts.views import RegisterView
 
+# Swagger / OpenAPI imports
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="category")
 router.register(r"products", ProductViewSet, basename="product")
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-Commerce API",
+        default_version="v1",
+        description="Backend API for the ALX Project Nexus E-Commerce catalog.",
+        terms_of_service="https://www.google.com/policies/terms/",  # optional
+        contact=openapi.Contact(email="support@example.com"),        # optional
+        license=openapi.License(name="MIT License"),                 # optional
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
+
     # User registration API
     path("api/auth/register/", RegisterView.as_view(), name="auth_register"),
 
     # JWT endpoints
     path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    # Swagger / OpenAPI
+    path("api/docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/schema.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]
